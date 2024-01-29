db914d1aea3c9a41f869b1848311b3efd2d7646c
tomulanovski <63614033+tomulanovski@users.noreply.github.com>
2024-01-29 12:22:34 +0200

    Update myshell.c

--- myshell.c
+++ myshell.c
@@ -5,20 +5,29 @@
 #include <sys/wait.h>
 #include <fcntl.h>
 
+#ifndef NULL
+    #define NULL 0
+#endif
+
 void execute(cmdLine *pCmdLine) {
 
     if (strcmp(pCmdLine->arguments[0], "cd") == 0) {
         // Handle 'cd' command
         if (chdir(pCmdLine->arguments[1]) == -1) {
             perror("Error in chdir");
+            exit(1);
         }
         return;
     }
-        if (strcmp(pCmdLine->arguments[0], "wakeup") == 0) {
+    if (strcmp(pCmdLine->arguments[0], "wakeup") == 0) {
         // Handle 'wakeup' command
         pid_t pid = atoi(pCmdLine->arguments[1]);
         if (kill(pid, SIGCONT) == -1) {
             perror("Error in wakeup");
+            exit(1);
+        }
+        else {
+            printf("wakeup succeded\n");
         }
         return;
     }
@@ -26,16 +35,20 @@ void execute(cmdLine *pCmdLine) {
     if (strcmp(pCmdLine->arguments[0], "nuke") == 0) {
         // Handle 'nuke' command
         pid_t pid = atoi(pCmdLine->arguments[1]);
-        if (kill(pid, SIGTSTP) == -1) {
-            perror("Error in nuke");
+        if (kill(pid, SIGINT) == -1) {
+            perror("Error in nuke\n");
+            exit(1);
         }
-        
+        else {
+            printf("nuke succeded\n");
+            
+        }
         return;
     }
 
     pid_t PID = fork();
-    if (PID==0) {
-    
+    if (PID==0) { // child
+  
         if (pCmdLine->inputRedirect) {
         int inputFile = open(pCmdLine->inputRedirect, O_RDONLY);
         if (inputFile == -1) {
@@ -61,7 +74,7 @@ void execute(cmdLine *pCmdLine) {
         }
     }
 
-    else if (PID > 0) {
+    else if (PID > 0) { //parent
         fprintf(stderr, "PID: %d\n", PID);
         fprintf(stderr, "Executing command: %s\n", pCmdLine->arguments[0]);
 
@@ -69,9 +82,14 @@ void execute(cmdLine *pCmdLine) {
             int status;
             waitpid(PID, &status, 0);
         }
+        else {
+            
+            fprintf(stderr, "Background process started with PID: %d\n", PID);
+            return;  
+        }
 
     }
-    else {
+    else { // error
             perror("Error in fork");
             exit(1);
         }
@@ -79,7 +97,7 @@ void execute(cmdLine *pCmdLine) {
 }
 
 
-int main() {
+int main(int argc, char **argv) {
     while(1) {
 
         char cwd[PATH_MAX];
@@ -89,22 +107,18 @@ int main() {
         } else {
             perror("error in getcwd function");
         }
-        if (strcmp(input, "quit") == 0) {
-            printf("Exiting myshell\n");
-            break;  // Exit the infinite loop if the user enters "quit"
-        }
         printf("enter input\n");
         if (fgets(input, sizeof(input), stdin) == NULL) {
             // Exit the loop on EOF
             printf("Exiting\n");
             exit(1);
         }
+
         cmdLine *lineFromInput = parseCmdLines(input);
 
-        if (strcmp(lineFromInput->arguments[0], "cd") == 0) {
-            if (chdir(lineFromInput->arguments[1]) == -1) {
-                perror("Error in chdir");
-            }
+         if (strcmp(input, "quit") == 0) {
+            printf("Exiting myshell\n");
+            break;  // Exit the infinite loop if the user enters "quit"
         }
         else {
             execute(lineFromInput);
@@ -114,6 +128,9 @@ int main() {
     }
     return 0;
 }
+
+
+
 
 
 
